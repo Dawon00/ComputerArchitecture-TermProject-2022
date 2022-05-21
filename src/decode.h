d3bdb249ee3557f297ec9bd3b391caa94a3a8759
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-05-21 16:09:57 +0900

    FEAT - 함수 추가

---
+++ src/decode.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// str -> unsigned int 변환
+unsigned int parseInt(char *str);
+
+// Endian 변환 (big -> little, little -> big)
+unsigned int convertEndian(unsigned int endian);
---
+++ src/interface.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+// 입력받은 명령어 포인터에 저장 (0: 성공, 1: 실패)
+int readCommand(char **command);
