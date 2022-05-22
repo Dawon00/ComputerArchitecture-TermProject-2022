9ccc3e7ccd00f929b522462d7c59232d1967a435
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-05-22 13:03:31 +0900

    ADD - 명령어 파싱 함수 추가

--- src/interface.h
+++ src/interface.h
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-// 입력받은 명령어 포인터에 저장 (0: 성공, 1: 실패)
-int readCommand(char **command);
+// 입력받은 문자열(명령어) 저장 후 문자열 길이 반환
+int readCommand(char *command);
+
+// 명령어 문자열 파싱 (argc 반환)
+int parseCommand(char *command, char **argv);
 
 // 프로그램 불러오기. l 명령어 입력 시 실행됨
 // 하는 일: 1. 파일을 읽기, 2. PC, 레지스터, 메모리 초기화 3. 파일 내용을 메모리에 적재
