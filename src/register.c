a00ed330f93f6c625eb83814bfffeac291c7cc59
Dawon00 <Dawon00@github.com>
2022-05-26 22:14:38 +0900

    FIX - register.c

--- src/register.c
+++ src/register.c
@@ -1,11 +1,14 @@
-#define REG_SIZE 32
+#define REGISTER_SIZE 32
+int PC = 0x400000;
+int HI = 0;
+int LO = 0;
 
-static char REGISTER[REG_SIZE][6] = {
+static char REGISTER[REGISTER_SIZE][6] = {
     "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$g7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra"};
 
 // access register interface(read & write)
 unsigned int REG(unsigned int A, unsigned int V, unsigned int nRW){
-  if (A > REG_SIZE - 1) {
+  if (A > REGISTER_SIZE - 1) {
 		printf("Wrong access to register\n");
 		exit(1);
 	}
@@ -20,7 +23,7 @@ unsigned int REG(unsigned int A, unsigned int V, unsigned int nRW){
 // 'r' instruction -> print register
 void showRegister(){
   	printf("==========[REGISTER]==========\n");
-	for (int i = 0; i < REG_SIZE; i++)
+	for (int i = 0; i < REGISTER_SIZE; i++)
 	{
 		printf("R%-2d = %x\n", i, REGISTER[i]);
 	}
@@ -34,10 +37,6 @@ void resetRegister();
 
 // 'sr' instruction -> register 값 설정
 void setRegister(unsigned int number, int value){
-  	printf("Enter register number and value(number value): ");
-		scanf_s("%d %x", &where, &value);
-		while (getchar() != '\n')
-			;
-		REG(where, value, 1);
+		REG(number, value, 1);
 }
 
